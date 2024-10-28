874a83680c5bf7c01c7c7611ee978f5859b9afd5
vannghia02004 <vuinghia32@gmail.com>
2024-10-28 10:43:57 +0700

    Initial commit

---
+++ Mavenproject1.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject1;
+
+public class Mavenproject1 {
+    public static void main(String[] args) {
+        System.out.println("Họ và tên: Le Van Nghia");
+        System.out.println("Lớp: TMP13as");
+        System.out.println("Mã số sinh viên: 29211153918");
+    }
+} git config --global user.name "vannghia02004"
